dd26bbb59329cd28ad8835449818901232b8794e
temuujinnn <temvkaa@gmail.com>
2021-05-24 17:16:11 +0800

    mmm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import "./App.css";
-import Login from "../loginPage";
-import Admin from "../adminpanelPage";
+import Login from "./pages/loginPage";
+import Admin from "./pages/adminpanelPage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route path="/admin">
-          <Admin />
-        </Route>
+        <Route exact path="/" component={Login} />
+        <Route path="/admin" component={Admin} />
       </Router>
     </div>
   );
